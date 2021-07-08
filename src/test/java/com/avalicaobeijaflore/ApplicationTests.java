2f62e7481a9526dca828a381463519e5c6ed2b26
edivaldo.melo <edivaldo.melo@basis.com.br>
2021-07-08 11:52:49 -0300

    add inicial commit

---
+++ src/test/java/com/avalicaobeijaflore/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avalicaobeijaflore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
