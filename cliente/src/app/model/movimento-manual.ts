18f98f2e9687647693bc3c6cfda0b3ac0a9d2770
edi <edimelo15@hotmail.com>
2021-07-25 21:10:33 -0300

    add

--- cliente/src/app/model/movimento-manual.ts
+++
@@ -1,9 +0,0 @@
-export class MovimentoManual {
-
-    mes: number = 0 ;
-    ano: number = 0;
-    
-    static fromJson(json: any): MovimentoManual {
-        return Object.assign(new MovimentoManual(), json);
-    }
-}
